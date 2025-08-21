ae18875414f774ebfb1433ec87056d9b23624466
Oleksii Pishchuhin <51528776+aliliolek@users.noreply.github.com>
2025-08-22 01:50:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from app.config import load_config
 
 def main() -> None:
     config = load_config()
-    client = get_api(config)
+    client = get_api(
+        api_key=config["api_key"],
+        api_secret=config["api_secret"],
+        testnet=config["testnet"],
+        recv_window=config["recv_window"],
+    )
     print(client.get_pending_orders())
 
 
