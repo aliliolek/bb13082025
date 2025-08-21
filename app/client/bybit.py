774f2144b60d95ed245f1f31b83afeca873cb6f6
Oleksii Pishchuhin <51528776+aliliolek@users.noreply.github.com>
2025-08-22 01:48:56 +0200

    Update bybit.py

--- app/client/bybit.py
+++ app/client/bybit.py
@@ -3,6 +3,11 @@
 from bybit_p2p import P2P
 
 
-def get_api(config: dict) -> P2P:
-    """Instantiate a Bybit P2P API client from config."""
-    return P2P(api_key=config["api_key"], api_secret=config["api_secret"])
+def get_api(*, api_key: str, api_secret: str, testnet: bool, recv_window: int) -> P2P:
+    """Instantiate a Bybit P2P API client."""
+    return P2P(
+        testnet=testnet,
+        api_key=api_key,
+        api_secret=api_secret,
+        recv_window=recv_window,
+    )
